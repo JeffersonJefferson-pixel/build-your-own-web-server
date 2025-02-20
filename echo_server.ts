c7776234f51dacbf2e23c4f29df46ee6fd9d9fd2
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2025-02-20 23:32:54 +0700

    add dynamic array
    split byte stream into messages

--- echo_server.ts
+++ echo_server.ts
@@ -1,5 +1,40 @@
 import * as net from "net";
 
+// Dynamic buffer is larger than the appended data to amortize the cost of copying.
+// this avoids appending data by concatentation which copy old data.
+type DynBuf = {
+    data: Buffer,
+    length: number, // actual data length.
+}
+
+// append data to dynamic buffer.
+function bufPush(buf: DynBuf, data: Buffer): void {
+    const newLen = buf.length + data.length;
+    // check if exceeds capacity.
+    if (buf.data.length < newLen) {
+        // expand capacity.
+        let cap = Math.max(buf.data.length, 32);
+        // double capacity until enough.
+        while (cap < newLen) {
+            cap *= 2;
+        }
+        // allocate new buffer.
+        const grown  = Buffer.alloc(cap);
+        buf.data.copy(grown, 0, 0);
+        buf.data = grown;
+    }
+    // append data to buffer.
+    data.copy(buf.data, buf.length, 0);
+    buf.length = newLen;
+}
+
+// pop data from buffer.
+function bufPop(buf: DynBuf, len: number): void {
+    // move data to the front.
+    buf.data.copyWithin(0, len, buf.length);
+    buf.length -= len;
+}
+
 type TCPConn = {
     socket: net.Socket;
     err: null | Error;
@@ -17,6 +52,7 @@ function soInit(socket: net.Socket): TCPConn {
     }
     socket.on('data', (data: Buffer) => {
         // pause data event.
+        // this implements backpressure.
         conn.socket.pause();
         // fulfill current read.
         conn.reader!.resolve(data);
@@ -57,6 +93,7 @@ function soRead(conn: TCPConn): Promise<Buffer> {
         // save callbacks to reader.
         conn.reader = {resolve, reject};
         // resume data event.
+        // only resumes when event handling completes.
         conn.socket.resume();
     });
 }
@@ -79,17 +116,53 @@ function soWrite(conn: TCPConn, data: Buffer): Promise<void> {
     });
 }
 
+
+// test if message is complete using delimiter.
+function cutMessage(buf: DynBuf): null | Buffer {
+    // find delimiter.
+    const idx = buf.data.subarray(0, buf.length).indexOf('\n');
+    if (idx < 0) {
+        // no complete message.
+        return null;
+    }
+    // make copy of message and pop data from buffer.
+    const msg = Buffer.from(buf.data.subarray(0, idx + 1));
+    bufPop(buf, idx + 1);
+    return msg;
+}
+
 async function serveClient(socket: net.Socket): Promise<void> {
     const conn: TCPConn = soInit(socket);
+    const buf: DynBuf = { data: Buffer.alloc(0), length: 0 };
     while (true) {
-        const data = await soRead(conn);
-        if (data.length === 0) {
-            console.log('end connection');
-            break;
+        // try to get message from buffer.
+        const msg: null | Buffer = cutMessage(buf);
+        if (!msg) {
+            // need more data.
+            const data: Buffer = await soRead(conn);
+            console.log('data', data);
+            bufPush(buf, data);
+            // check EOF.
+            if (data.length === 0) {
+                console.log('end connection');
+                return;
+            }
+            // got some data, try again.
+            continue;
         }
-
-        console.log('data', data);
-        await soWrite(conn, data);
+        
+        // process message.
+        if (msg.equals(Buffer.from('quit\n'))) {
+            await soWrite(conn, Buffer.from('Bye.\n'));
+            socket.destroy();
+            return;
+        } else {
+            const reply = Buffer.concat([Buffer.from('Echo: '), msg]);
+            // wait for write to complete.
+            // while application is waiting, it cannot produce. 
+            // this avoids unbounded queue.
+            await soWrite(conn, reply);
+        }
     }
 }
 
